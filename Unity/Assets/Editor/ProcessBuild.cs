dcec423bb5ea25500c99bec2f096a80d47d570c4
xuefei <awnuxcvbn@163.com>
2023-03-02 11:57:02 +0800

    iOS打包处理

---
+++ Unity/Assets/Editor/ProcessBuild.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+using UnityEditor.iOS.Xcode.Extensions;
+#endif
+
+public class ProcessBuild
+{
+#if UNITY_IOS
+    [PostProcessBuildAttribute(88)]
+    public static void onPostProcessBuild(BuildTarget buildTarget, string path)
+    {
+        if (buildTarget != BuildTarget.iOS)
+        {
+            Debug.LogWarning("Target is not iPhone. XCodePostProcess will not run");
+            return;
+        }
+        //导入文件
+        PBXProject proj = new PBXProject();
+        string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        proj.ReadFromFile(projPath);
+        //xcode Target
+        string target = proj.GetUnityMainTargetGuid();
+        string unityTarget = proj.GetUnityFrameworkTargetGuid(); 
+
+    #region GMESDK相关
+        const string defaultLocationInProj = "IJKPlayer/Plugins/iOS/IJKPlayer";
+        const string IJKMediaFrameworkName = "IJKMediaFramework.framework"; 
+
+        string IJKMediaFrameworkPath = Path.Combine(defaultLocationInProj, IJKMediaFrameworkName); 
+
+        string fileGuid = proj.AddFile(IJKMediaFrameworkPath, "Frameworks/" + IJKMediaFrameworkPath, PBXSourceTree.Sdk);
+        PBXProjectExtensions.AddFileToEmbedFrameworks(proj, target, fileGuid); 
+
+        proj.SetBuildProperty(target, "LD_RUNPATH_SEARCH_PATHS", "$(inherited) @executable_path/Frameworks"); 
+
+    #endregion
+
+        string fileGuidLibz = proj.AddFile("usr/lib/libz.tbd", "Libraries/libz.tbd", PBXSourceTree.Sdk);
+        proj.AddFileToBuild(target, fileGuidLibz);
+        proj.AddFileToBuild(unityTarget, fileGuidLibz); 
+         
+        // rewrite to file
+        File.WriteAllText(projPath, proj.WriteToString()); 
+    }
+#endif
+}
