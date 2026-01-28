da905d6a4bd35a51d79957bf66de2d739f2e0b38
xue-fei <awnuxcvbn@163.com>
2026-01-28 17:19:35 +0800

    直接使用https://github.com/debugly/ijkplayer.git的编译文件，画面颜色不正

--- Android/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
+++ Android/ijkplayer-java/src/main/java/tv/danmaku/ijk/media/player/IjkMediaPlayer.java
@@ -187,8 +187,8 @@ public final class IjkMediaPlayer extends AbstractMediaPlayer {
                 if (libLoader == null)
                     libLoader = sLocalLibLoader;
 
-                libLoader.loadLibrary("ijkffmpeg");
-                libLoader.loadLibrary("ijksdl");
+                //libLoader.loadLibrary("ijkffmpeg");
+                //libLoader.loadLibrary("ijksdl");
                 libLoader.loadLibrary("ijkplayer");
                 mIsLibLoaded = true;
             }
